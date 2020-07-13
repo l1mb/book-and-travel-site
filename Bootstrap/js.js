d74294f4b4d219681feb5c3d2faeaa627b7da224
l1mb <57047025+l1mb@users.noreply.github.com>
2020-07-13 12:10:22 +0300

    Delete js.js

--- Bootstrap/js.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-	// плавное перемещение страницы к нужному блоку
-	$("nav div a").on("click", function(e) {
-    e.preventDefault();
-		elementClick = $(this).attr("href");
-		destination = $(elementClick).offset().top;
-		$("body,html").animate({scrollTop: (destination-60)}, 800);
-	});
-});
