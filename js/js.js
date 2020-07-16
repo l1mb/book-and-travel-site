4fca2c7c4ada68ed59360cee3f27092d518f79d8
l1mb <57047025+l1mb@users.noreply.github.com>
2020-07-16 12:41:11 +0300

    Add files via upload
    phone.mask, requirement of email and textarea was added. pass input was removed.

--- js/js.js
+++ js/js.js
@@ -19,3 +19,16 @@ $(document).ready(function(){
 	return false;
 	});
 });
+
+
+
+
+jQuery(function($){
+	$('.date').mask('99/99/9999');
+	$('.phone').mask('+9 (999) 999-9999');
+	$('.phoneext').mask("(999) 999-9999? x99999");
+	$(".tin").mask("99-9999999");
+	$(".ssn").mask("999-99-9999");
+	$(".product").mask("a*-999-a999");
+	$(".eyescript").mask("~9.99 ~9.99 999");
+});
